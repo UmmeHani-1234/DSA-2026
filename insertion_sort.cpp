10c008e94dceaa5866876666847887d082050351
Hani <ummehanishaikh056@gmail.com>
2026-08-04 00:14:45 +0530

    Push pending changes

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -2,34 +2,21 @@
 using namespace std;
 
 int main() {
-    int arr[] = {1, 0, 1, 0, 1};
+    int arr[] = {8, 6, 3, 4, 9};
 
     int size = sizeof(arr) / sizeof(arr[0]);
 
-    int left = 0;
-    int right = size - 1;
+    for (int i = 1; i < size; i++) {
+        for (int j = i; j > 0; j--) {
+            if (arr[j] < arr[j - 1]) {
+                swap(arr[j], arr[j - 1]);
+            }
+        }
+    }
 
     for (int i = 0; i < size; i++) {
-       
+        cout << arr[i] << " ";
     }
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

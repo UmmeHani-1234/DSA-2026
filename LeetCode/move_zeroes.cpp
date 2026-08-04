3b13ffdc0e5189ef3d753c3f3070a97f0aa71ae0
Hani <ummehanishaikh056@gmail.com>
2026-08-04 15:55:39 +0530

    moving zeroes to end

---
+++ LeetCode/move_zeroes.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[6] = {1, 3,0, 5,0,9};
+
+
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int i = 0;
+    while (i <= size){
+        if (arr[i] == 0){
+            for (int j = i; j < size-1; j++){
+                arr[j] = arr[j+1];
+            }
+            arr[size - 1] = 0;
+            i++;
+        }
+            else {
+                i++;
+            }
+        
+    }
+   
+
+    for (int i: arr){
+        cout << i << " ";
+    }
+
+    return 0;
+}

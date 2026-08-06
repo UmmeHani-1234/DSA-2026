c0beda3619f7957779791c64eb643096be4bfbcf
UmmeHani-1234 <ummehanishaikh056@gmail.com>
2026-08-06 23:03:56 +0530

    reversing each element of a string

---
+++ Char Array/reverse_words.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    string s = "my name is hani";
+    string ans;
+    string temp;
+
+    int size = s.size();
+    int i = 0;
+
+    while (i < size) {
+        temp.clear();
+
+        while (i < size && s[i] != ' ') {
+            temp.push_back(s[i]);
+            i++;
+        }
+
+        reverse(temp.begin(), temp.end());
+
+        if (!ans.empty())
+            ans += " ";
+
+        ans += temp;
+
+        i++; // Skip the space
+    }
+
+    cout << ans;
+
+    return 0;
+}

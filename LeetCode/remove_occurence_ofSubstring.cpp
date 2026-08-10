4f159ae84e0515d3293a93de4f19d78ea69bc0cd
UmmeHani-1234 <ummehanishaikh056@gmail.com>
2026-08-10 23:10:23 +0530

    removing all the occurence of a substring from a string

---
+++ LeetCode/remove_occurence_ofSubstring.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    string str = "abcccdccicbsci";
+    string sub = "ccc";
+
+    int i = 0;
+    string temp;
+
+    while (i < str.size()) {
+        int j = 0;
+        int count = 0;
+        int start = i;
+        int k = i;
+
+        temp = "";
+
+        if (str[i] == sub[j]) {
+
+            while (k < str.size() && j < sub.size()) {
+
+                if (str[k] == sub[j]) {
+                    temp.push_back(str[k]);
+                    k++;
+                    j++;
+                    count++;
+                }
+                else {
+                    break;
+                }
+            }
+
+            if (temp == sub) {
+                str.erase(start, count);
+                i = start;
+            }
+            else {
+                i++;
+            }
+        }
+        else {
+            i++;
+        }
+    }
+
+    cout << str;
+
+    return 0;
+}

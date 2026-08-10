ac077b2cb3de2a6f5c676c2ab4534e65cf0277aa
UmmeHani-1234 <ummehanishaikh056@gmail.com>
2026-08-10 23:34:47 +0530

    stored the leet code version of code

--- LeetCode/remove_occurence_ofSubstring.cpp
+++ LeetCode/remove_occurence_ofSubstring.cpp
@@ -49,3 +49,57 @@ int main() {
 
     return 0;
 }
+// leet code version
+/*
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    string str = "abcccdccicbsci";
+    string sub = "ccc";
+    cout<< "the original string is: "<<str<<endl;
+    int i = 0;
+    string temp;
+
+    while (i < str.size()) {
+        int j = 0;
+        int count = 0;
+        int start = i;
+        int k = i;
+
+        temp = "";
+
+        if (str[i] == sub[j]) {
+
+            while (k < str.size() && j < sub.size()) {
+
+                if (str[k] == sub[j]) {
+                    temp.push_back(str[k]);
+                    k++;
+                    j++;
+                    count++;
+                }
+                else {
+                    break;
+                }
+            }
+
+            if (temp == sub) {
+                str.erase(start, count);
+                i = start;
+            }
+            else {
+                i++;
+            }
+        }
+        else {
+            i++;
+        }
+    }
+    
+    cout << "the string after removing the occurence is: "<<str;
+
+    return 0;
+}
+*/

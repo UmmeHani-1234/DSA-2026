0969b7b34b6936a8dc0b130462bcbe2c05981509
Awesome Hani <ummehanishaikh056@email.com>
2026-08-15 18:29:44 +0530

    removing adjacent character from a string

---
+++ LeetCode/remove_adjacentChar.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+
+        int i = 0;
+        int size = s.length();
+        while (i < size - 1) {
+
+            if (s[i] == s[i + 1]) {
+                s.erase(i, 2);
+
+                if (i > 0)
+                    i--;
+            }
+            else {
+                i++;
+            }
+        }
+        return s;
+    }
+};

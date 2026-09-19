c29a6a746731facf2280c933756d3b0a2c837a95
Awesome Hani <ummehanishaikh056@email.com>
2026-09-20 00:23:10 +0530

    double pointers

---
+++ Char Array/doubtle_pointer.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void update(int **ptr2) {
+    *ptr2 = *ptr2 + 1;
+    **ptr2 = **ptr2 + 1;
+}
+
+int main() {
+    int i = 5;
+    int *ptr = &i;
+    int **ptr2 = &ptr;
+
+    cout << "before" << endl;
+    cout << ptr << endl;      // address of i
+    cout << *ptr << endl;     // value of i
+    cout << ptr2 << endl;     // address of ptr
+    cout << *ptr2 << endl;    // address of i
+    cout << **ptr2 << endl;   // value of i
+
+    update(ptr2);
+
+    cout << "after" << endl;
+    cout << ptr << endl;      // address of i
+    cout << *ptr << endl;     // value of i
+    cout << ptr2 << endl;     // address of ptr
+    cout << *ptr2 << endl;    // address of i
+    cout << **ptr2 << endl;   // value of i
+
+    return 0;
+}
